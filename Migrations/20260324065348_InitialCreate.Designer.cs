91ccf21fb84a20a6d973463928377047e0364ee9
TemuSenior <aegsiti@gmail.com>
2026-03-24 00:54:47 -0600

    Regenerate migrations for PostgreSQL

--- Migrations/20260324065348_InitialCreate.Designer.cs
+++ Migrations/20260324065348_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using WordGuessAPI.Data;
 namespace WordGuessAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260324055619_InitialCreate")]
+    [Migration("20260324065348_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
