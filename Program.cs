20a2c7ee7ca39b0eef06d00045fcabde1c71a4b0
TemuSenior <aegsiti@gmail.com>
2026-03-23 22:21:21 -0600

    Use EnsureCreated instead of Migrate

--- Program.cs
+++ Program.cs
@@ -85,7 +85,7 @@ app.MapControllers();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
+    db.Database.EnsureCreated();   // <-- nueva línea
 }
 
 app.Run();
